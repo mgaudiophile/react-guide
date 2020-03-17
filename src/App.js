098d3ca116dbe3f4433022bfd507b6f23e08baa0
mgaudiophile <mgaudiofile@gmail.com>
2020-03-16 22:58:42 -0600

    working with state and props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 23 },
+      { name: 'Manu', age: 14 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maxamilian';
+    this.setState({
+      persons: [
+        { name: 'Maxamilian', age: 23 },
+        { name: 'Manu', age: 114 },
+        { name: 'Stephanie', age: 57 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <p>class is not used in JSX</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing!</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
