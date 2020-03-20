bffdad1c8e7705c22acd83539b2890df181d8905
mgaudiophile <mgaudiofile@gmail.com>
2020-03-19 18:11:06 -0600

    render content dynamically

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 14 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'Some other value'
+    otherState: 'Some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,24 +35,33 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   // bind is more efficient than anonymous function
   render () {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>class is not used in JSX</p>
-        <button onClick={ () => this.switchNameHandler('Better')} >Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          changed={this.nameChangedHandler}
-        />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} 
-          click={this.switchNameHandler.bind(this, 'Secret')}>My Hobbies: Racing!
-        </Person>
+        <button onClick={this.togglePersonsHandler} >Switch Name</button>
+        { this.state.showPersons === true ? 
+          <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+            changed={this.nameChangedHandler}
+          />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} 
+            click={this.switchNameHandler.bind(this, 'Secret')}>My Hobbies: Racing!
+          </Person>
+        </div> : null
+        }
       </div>
     );
   }
